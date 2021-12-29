ab0f99a8ddc27113f224e4fae0e82301dc201039
Daste <stefankar1000@gmail.com>
2021-12-29 22:04:01 +0100

    Diesel init

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,6 +2,10 @@ use std::{env, error::Error};
 
 use actix_web::{middleware::Logger, App, HttpServer};
 use backend::config;
+use diesel::{
+    r2d2::{ConnectionManager, Pool},
+    PgConnection,
+};
 use dotenv::dotenv;
 
 #[actix_web::main]
@@ -11,10 +15,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let bind_url = format!("{}:{}", env::var("HOST")?, env::var("PORT")?);
 
-    HttpServer::new(move || App::new().wrap(Logger::default()).configure(config))
-        .bind(bind_url)?
-        .run()
-        .await?;
+    let database_url = env::var("DATABASE_URL")?;
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = Pool::builder().build(manager)?;
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .data(pool.clone())
+            .configure(config)
+    })
+    .bind(bind_url)?
+    .run()
+    .await?;
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 pub mod routes;
 
+#[macro_use]
+extern crate diesel;
 use actix_web::web::ServiceConfig;
+use diesel::r2d2;
+
+type DbPool = r2d2::Pool<r2d2::ConnectionManager<diesel::PgConnection>>;
 
 pub fn config(cfg: &mut ServiceConfig) {}
