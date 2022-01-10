22d1810b81ee2542dddc1e7a76d99f47341e3720
Daste <stefankar1000@gmail.com>
2022-01-10 22:28:03 +0100

    User extrator for protected endpoints

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,12 @@ pub mod schema;
 
 #[macro_use]
 extern crate diesel;
-use actix_web::web::{scope, ServiceConfig};
+use actix_web::{
+    web::{scope, ServiceConfig},
+    HttpRequest,
+};
 use diesel::r2d2;
+use jsonwebtoken::{decode, DecodingKey, Validation};
 use serde::{Deserialize, Serialize};
 
 use routes::{auth, users};
@@ -22,4 +26,33 @@ pub struct Claims {
     sub: String,
     iat: usize,
     exp: usize,
+}
+
+impl Claims {
+    pub fn from_request(req: &HttpRequest) -> Result<Self, ()> {
+        let token = req
+            .headers()
+            .get("authorization")
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .replace("Bearer ", "");
+
+        let validation = Validation {
+            leeway: 60,
+            ..Default::default()
+        };
+        let token_data = decode::<Claims>(
+            &token.as_str(),
+            &DecodingKey::from_secret("supersecret".as_bytes()),
+            &validation,
+        )
+        .unwrap();
+
+        // TODO: Validation
+
+        // TODO: Error handling
+
+        Ok(token_data.claims)
+    }
 }
--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,14 @@
-use crate::schema::users;
+use std::{
+    future::{ready, Ready},
+    str::FromStr,
+};
+
+use actix_web::{dev::Payload, web::Data, Error, FromRequest, HttpRequest};
+use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+
+use crate::{schema::users, Claims, DbPool};
 
 #[derive(Debug, Queryable, Insertable, Serialize, Deserialize)]
 #[table_name = "users"]
@@ -10,4 +18,26 @@ pub struct User {
     pub mail: String,
     #[serde(skip_serializing)]
     pub password: String,
+}
+
+impl FromRequest for User {
+    type Config = ();
+    type Error = Error;
+    type Future = Ready<Result<User, Error>>;
+
+    fn from_request(req: &HttpRequest, _pld: &mut Payload) -> Self::Future {
+        let claims = Claims::from_request(req).unwrap();
+
+        // TODO: Verify claims
+
+        let pool = req.app_data::<Data<DbPool>>().unwrap();
+        let conn = pool.get().unwrap();
+
+        let user_id = Uuid::from_str(claims.sub.as_str()).unwrap();
+        let user = users::table.find(user_id).first::<User>(&conn).unwrap();
+
+        // TODO: Error handling
+
+        ready(Ok(user))
+    }
 }
