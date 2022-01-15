022468a454469fd56175fe5f7e0aa52586b56c7b
Daste <stefankar1000@gmail.com>
2022-01-16 00:52:02 +0100

    Main: Clean up error handling

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,4 @@
-use std::{env, error::Error};
+use std::{env, io};
 
 use actix_web::{middleware::Logger, App, HttpServer};
 use backend::{config, JWTConfig};
@@ -9,15 +9,21 @@ use diesel::{
 use dotenv::dotenv;
 
 #[actix_web::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> io::Result<()> {
     dotenv().ok();
     env_logger::init();
 
-    let bind_url = format!("{}:{}", env::var("HOST")?, env::var("PORT")?);
+    let bind_url = format!(
+        "{}:{}",
+        env::var("HOST").expect("HOST must be set"),
+        env::var("PORT").expect("PORT must be set")
+    );
 
-    let database_url = env::var("DATABASE_URL")?;
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
-    let pool = Pool::builder().build(manager)?;
+    let pool = Pool::builder()
+        .build(manager)
+        .expect("Could not create database connection pool");
 
     let jwt_secret = env::var("JWT_SECRET").expect("JWT_SECRET must be set");
     let jwt_expiry = env::var("JWT_EXPIRY").expect("JWT_EXPIRY must be set");
@@ -32,7 +38,5 @@ async fn main() -> Result<(), Box<dyn Error>> {
     })
     .bind(bind_url)?
     .run()
-    .await?;
-
-    Ok(())
+    .await
 }
