977c74318639d01f65a317d5f9feddeee4e3d223
Daste <stefankar1000@gmail.com>
2022-01-30 01:00:59 +0100

    Make route modules private

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,7 @@
 use actix_web::web::{scope, ServiceConfig};
 
-pub mod auth;
-pub mod users;
+mod auth;
+mod users;
 
 pub fn config(cfg: &mut ServiceConfig) {
     cfg.service(scope("/auth").configure(auth::config))
