3f6bb175ee12ec3ba15a754b8678eba87f0b45b4
Daste <stefankar1000@gmail.com>
2022-02-03 14:54:04 +0100

    Boards: Move ORM logic into impl Board

--- src/routes/boards.rs
+++ src/routes/boards.rs
@@ -8,8 +8,8 @@ use uuid::Uuid;
 
 use crate::{
     errors::ServiceError,
+    get_conn,
     models::{Board, BoardUpdate, User},
-    schema::boards,
     DbPool,
 };
 
@@ -27,19 +27,8 @@ async fn new_board(
     user: User,
     Json(data): Json<Board>,
 ) -> Result<HttpResponse, Error> {
-    let conn = pool.get().unwrap();
-
-    let board = Board {
-        id: Uuid::new_v4(),
-        owner: user.id,
-        name: data.name,
-        description: data.description,
-    };
-
-    diesel::insert_into(boards::table)
-        .values(&board)
-        .execute(&conn)
-        .map_err(|_| ServiceError::InternalServerError)?;
+    let board = Board::new(&user, data.name, data.description);
+    board.save(&pool)?;
 
     Ok(HttpResponse::Created()
         .header("Location", format!("/{}", board.id))
@@ -48,7 +37,7 @@ async fn new_board(
 
 #[get("/me")]
 async fn my_boards(pool: Data<DbPool>, user: User) -> Result<HttpResponse, Error> {
-    let conn = pool.get().unwrap();
+    let conn = get_conn(&pool)?;
 
     let boards = Board::belonging_to(&user)
         .load::<Board>(&conn)
@@ -59,19 +48,11 @@ async fn my_boards(pool: Data<DbPool>, user: User) -> Result<HttpResponse, Error
 
 #[get("/{board_id}")]
 async fn get_board(pool: Data<DbPool>, Path(board_id): Path<Uuid>) -> Result<HttpResponse, Error> {
-    let conn = pool.get().unwrap();
-
-    let board = boards::table
-        .find(board_id)
-        .first::<Board>(&conn)
-        .optional()
-        .map_err(|_| ServiceError::InternalServerError)?;
-
-    Ok(if let Some(board) = board {
-        HttpResponse::Ok().json(board)
+    if let Some(board) = Board::find(&pool, board_id)? {
+        Ok(HttpResponse::Ok().json(board))
     } else {
-        HttpResponse::NotFound().finish()
-    })
+        Err(HttpResponse::NotFound().finish())?
+    }
 }
 
 #[patch("/{board_id}")]
@@ -81,29 +62,18 @@ async fn patch_board(
     Path(board_id): Path<Uuid>,
     Json(mut data): Json<BoardUpdate>,
 ) -> Result<HttpResponse, Error> {
-    let conn = pool.get().unwrap();
-
-    if data.name == None && data.description == None {
+    if data.is_empty() {
         Err(ServiceError::EmptyUpdate)?
     }
 
     data.id = board_id;
 
-    let board = boards::table
-        .find(board_id)
-        .first::<Board>(&conn)
-        .optional()
-        .map_err(|_| ServiceError::InternalServerError)?;
-
-    if let Some(board) = board {
+    if let Some(board) = Board::find(&pool, board_id)? {
         if board.owner != user.id {
             Err(HttpResponse::Unauthorized().finish())?
         }
 
-        let board = diesel::update(&board)
-            .set(&data)
-            .get_result::<Board>(&conn)
-            .map_err(|_| ServiceError::InternalServerError)?;
+        let board = board.update(&pool, data)?;
 
         Ok(HttpResponse::Ok().json(board))
     } else {
@@ -117,22 +87,12 @@ async fn delete_board(
     user: User,
     Path(board_id): Path<Uuid>,
 ) -> Result<HttpResponse, Error> {
-    let conn = pool.get().unwrap();
-
-    let board = boards::table
-        .find(board_id)
-        .first::<Board>(&conn)
-        .optional()
-        .map_err(|_| ServiceError::InternalServerError)?;
-
-    if let Some(board) = board {
+    if let Some(board) = Board::find(&pool, board_id)? {
         if board.owner != user.id {
             Err(HttpResponse::Unauthorized().finish())?
         }
 
-        diesel::delete(&board)
-            .execute(&conn)
-            .map_err(|_| ServiceError::InternalServerError)?;
+        board.delete(&pool)?;
     }
 
     Ok(HttpResponse::NoContent().finish())
